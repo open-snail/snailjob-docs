5774144ce26c8b124372ccee2101882c76538fe0
byteblogs168 <598092184@qq.com>
2023-07-07 09:18:12 +0800

    feat: 2.0。0 调整导航栏配置

---
+++ docs/.vuepress/config/nav.js
@@ -0,0 +1,41 @@
+module.exports = [
+    {text: '首页', link: '/'},
+    {
+        text: '文档', items: [
+            {
+                text: '最新特性', items: [
+                    {text: '最新特性v2.0.x', link: '/pages/f203ee/'},
+                ]
+            },
+            {
+                text: 'v2.0.x', items: [
+                    {text: '系统介绍', link: '/pages/d1d1da/'},
+                    {text: '使用说明', link: '/pages/406a66/'},
+                ]
+            }
+        ]
+    },
+    {
+        text: '问答', items: [
+            {text: '常见问题', link: ''},
+            {
+                text: '专题解释', items: [
+                    {text: '', link: ''},
+                ]
+            },
+        ]
+    },
+    {
+        text: '关于项目', items: [
+            {text: '项目介绍', link: ''},
+            {text: '项目成员', link: ''},
+            {text: '更新记录', link: '/pages/bbdaf6/'},
+            {text: '参与开发', link: ''},
+        ]
+    },
+    {text: '加入群聊', link: ''},
+    {text: '用户', link: ''},
+    { text: '优秀文章', link: '/pages/db78e2/' },
+    { text: 'Gitee', link: 'https://gitee.com/aizuda/easy-retry' },
+    { text: 'Github', link: 'https://github.com/aizuda/easy-retry' }
+];
