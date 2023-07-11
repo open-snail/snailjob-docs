c9101b0c1fb542cab05700cdb4818cd8fbebdcff
byteblogs168 <598092184@qq.com>
2023-07-11 10:24:35 +0800

    feat: 2.0.0 变更备案号

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -134,7 +134,7 @@ module.exports = {
             copyrightInfo: [
                 '<a href="http://aizuda.com/" target="_blank" style="font-weight:bold">Team Aizudai</a>',
                 ' | ',
-                '<a href="http://beian.miit.gov.cn/" target=_blank>皖ICP备16014822号</a>',
+                '<a href="http://beian.miit.gov.cn/" target=_blank>皖ICP备16014822号-7</a>',
                 '</p>',
             ].join('')
         },
@@ -171,7 +171,7 @@ module.exports = {
         [
             'vuepress-plugin-baidu-tongji', // 百度统计
             {
-                hm: baiduCode || '01293bffa6c3962016c08ba685c79d78'
+                hm: '2c305d1f977ecf38bd04a352782d724b'
             }
         ],
 
